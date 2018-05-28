d60bfdb81651322235fc664f8fa9563857c084dd
Toccata Yi <Xingyu.Yi@emc.com>
2018-05-28 17:55:19 +0800

    initial commit of message feature

---
+++ backend/entities/message/model.js
@@ -0,0 +1,17 @@
+/**
+ * opinion model
+ */
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+  type: String,
+  discussion_id: mongoose.Schema.ObjectId,
+  discussion: { type: mongoose.Schema.ObjectId, ref: 'discussion' },
+  to: { type: mongoose.Schema.ObjectId, ref: 'user' },
+  from: { type: mongoose.Schema.ObjectId, ref: 'user' },
+  date: Date,
+  content: Object,
+  read: Boolean,
+});
+
+module.exports = mongoose.model('message', opinionSchema);
---
+++ frontend/Views/Messages/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import classnames from 'classnames';
+import appLayout from 'SharedStyles/appLayout.css';
+import styles from './styles.css';
+
+// components used in this view
+import Profile from 'Components/UserProfile/Profile';
+
+class Message extends Component {
+	ComponentDidMount() {
+		1
+	}
+
+	render() {
+		1
+		return ();
+	}
+}
